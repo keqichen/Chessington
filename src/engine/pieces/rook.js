d3011604bb603be26d7146c463379f50e55f54bd
Keqi Chen <ckq009@gmail.com>
2023-01-24 14:24:06 +0000

    Rook.js passes 14 tests

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -9,6 +9,13 @@ export default class Rook extends Piece {
         super(player);
     }
 
+    checkForPiece (board,row,col){
+        if (board.getPiece(Square.at(row,col)) === undefined) {
+            return false;
+        } 
+        return true;   
+    }
+
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
@@ -20,26 +27,50 @@ export default class Rook extends Piece {
         // In other words, either 'break' from that for loop, OR set the increment counter to the end 
         // In this case, if the rook is in the middle of the board, there will be four directions. We need a for loop per direction, or to somehow merge it with the below (e.g. by skipping to the location.row/location.col, then skipping on)
 
-        function checkForPiece (getPieceFunc){
-            if (typeof getPieceFunc == "undefined") {
-                return false
-            } else {
-                return true
-            }
+        
+
+        // our piece (4,4); blocking(4.6); not include (4,7) or after;
+        // for (let i = 0; i<8; i++) {
+        //     if (i != location.row ){
+        //     //    console.log(checkForPiece(board.getPiece(Square.at(i,location.col))))
             
+        //     moves.push(Square.at(i, location.col));
+        //     }
+        //     if (i != location.col )
+        //     {
+        //     // console.log(checkForPiece(board.getPiece(Square.at(location.row,i))))
+        //     moves.push(Square.at(location.row, i));
+
+        //     }
+        // }
+
+        //1.forward 
+        for (let i=location.row+1; i<8; i++){
+            moves.push(Square.at(i, location.col));
+            if (this.checkForPiece(board,i,location.col)){
+                break;
+            }
         }
 
-        for (let i = 0; i<8; i++) {
-            if (i != location.row ){
-            //    console.log(checkForPiece(board.getPiece(Square.at(i,location.col))))
+        //2.backward
+        for (let i=location.row-1; i>=0; i--){
             moves.push(Square.at(i, location.col));
+            if (this.checkForPiece(board,i,location.col)){
+                break;
             }
-            if (i != location.col )
-            {
-            // console.log(checkForPiece(board.getPiece(Square.at(location.row,i))))
+        }
+    
+        //3.left
+        for (let i = location.col - 1; i>=0;i--) {
             moves.push(Square.at(location.row, i));
-
-            }
+            if (this.checkForPiece(board,location.row,i)) break;
+        
+        }
+        //4.rigth
+        for (let i = location.col + 1; i<8;i++) {
+            moves.push(Square.at(location.row, i));
+            if (this.checkForPiece(board,location.row,i)) break;
+        
         }
 
         return moves
