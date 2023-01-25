d043ac69800c8a7a65e6e6ef72ec9ef200547f5b
Keqi Chen <ckq009@gmail.com>
2023-01-25 00:01:49 +0000

    Pass cannot leave the board test

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -10,20 +10,24 @@ export default class King extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        //moves holds an array of all possible coordinates the piece can move to - which are pushed from below. For pieces this array will be longer
-        //for loops would be better to generate all possible moves
-        // deal with all possible moving first? Or think about how a 'checkMove' function might stop pieces from going to taken squares (or not moving beyond them)!
+        
         const moves = []
-        //defining moves going clockwise from the first i.e. directly forward, diagonally up and right, right etc 
-        moves.push(Square.at(location.row + 1, location.col));
-        moves.push(Square.at(location.row + 1, location.col + 1));
-        moves.push(Square.at(location.row, location.col + 1));
-        moves.push(Square.at(location.row - 1, location.col + 1));
-        moves.push(Square.at(location.row - 1, location.col));
-        moves.push(Square.at(location.row - 1, location.col - 1));
-        moves.push(Square.at(location.row, location.col - 1));
-        moves.push(Square.at(location.row + 1, location.col - 1));
-        //below for if this.player === player.BLACK
+        
+        function checkPieceOnboard(row,col){
+            if (row<8 && row >=0 && col < 8 && col >= 0){
+                moves.push(Square.at(row,col))
+            } 
+        }
+
+        checkPieceOnboard(location.row + 1, location.col);
+        checkPieceOnboard(location.row + 1, location.col + 1);
+        checkPieceOnboard(location.row, location.col + 1);
+        checkPieceOnboard(location.row - 1, location.col + 1);
+        checkPieceOnboard(location.row - 1, location.col);
+        checkPieceOnboard(location.row - 1, location.col - 1);
+        checkPieceOnboard(location.row, location.col - 1);
+        checkPieceOnboard(location.row + 1, location.col - 1);
+        
 
         return moves
     }
