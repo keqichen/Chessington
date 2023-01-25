d48fee4d83ec5415e3a009afab72934bf8929ddb
Keqi Chen <ckq009@gmail.com>
2023-01-25 00:06:05 +0000

    Comment on the next step for Queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 
+//Next step: try to let Queen inherits properties from bishop and rook
 export default class Queen extends Piece {
     constructor(player) {
         super(player);
